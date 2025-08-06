05362d0814088c440b96ccf0be43826d4874a708
Sakshii44 <sakshi4403@gmail.com>
2025-08-07 01:21:25 +0530

    Initial commit

--- app.py
+++ app.py
@@ -8,14 +8,17 @@ import numpy as np
 model = joblib.load("iris_rf_model.joblib")
 target_names = joblib.load("target_names.joblib")
 
+#create API instance
 app = FastAPI(title="Iris Species Predictor API")
 
+#model for input
 class IrisRequest(BaseModel):
     sepal_length: float
     sepal_width: float
     petal_length: float
     petal_width: float
 
+#creating post endpoint
 @app.post("/predict")
 def predict_species(data: IrisRequest):
     input_features = np.array([[data.sepal_length, data.sepal_width, data.petal_length, data.petal_width]])
